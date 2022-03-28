7662dd6cdb3dcdfb8e0f29e6211da015cc4abbff
KimAgel2002 <cristinaionescu2002@gmail.com>
2022-03-28 22:16:32 +0300

    stage 1

--- src/Stage1.java
+++ src/Stage1.java
@@ -3,19 +3,19 @@ public class Stage1 {
     {
         for(int i=1; i<=100; i++){
                 //stage 1
-//            if (i%3==0 && i%5==0)
-//                System.out.println("FizzBuzz");
-//            else if(i%3==0)
-//                System.out.println("Fizz");
-//            else if(i%5==0)
-//                System.out.println("Buzz");
-//            else
-//                System.out.print(i + System.lineSeparator());
-                //stage 2
-            if(i%7==0)
-                System.out.print("Rizz, ");
+            if (i%3==0 && i%5==0)
+                System.out.println("FizzBuzz");
+            else if(i%3==0)
+                System.out.println("Fizz");
+            else if(i%5==0)
+                System.out.println("Buzz");
             else
-                System.out.print(1+", ");
+                System.out.print(i + System.lineSeparator());
+                //stage 2
+//            if(i%7==0)
+//                System.out.print("Rizz, ");
+//            else
+//                System.out.print(1+", ");
 
         }
     }
