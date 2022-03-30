7de4e7f0dbac1e1a9f2ba4f7d874183e44e46602
KimAgel2002 <cristinaionescu2002@gmail.com>
2022-03-30 14:22:22 +0300

    ambele stage-uri rezolvate

---
+++ src/FizzBuzz.java
@@ -0,0 +1,27 @@
+public class FizzBuzz {
+    public static void main(String args[]) {
+        int ok = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                ok = 1;
+                System.out.print("Fizz");
+            }
+            if (i % 5 == 0) {
+                ok = 1;
+                System.out.print("Buzz");
+            }
+            if (i % 7 == 0) {
+                ok = 1;
+                System.out.print("Rizz, ");
+            }
+            if (i % 11 == 0) {
+                ok = 1;
+                System.out.print("Jazz");
+            }
+            if (ok == 0)
+                System.out.print(i);
+            ok = 0;
+            System.out.print(", ");
+        }
+    }
+}
